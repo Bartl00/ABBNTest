6170d80e00ded4cc1f9b1be10a323be613603f16
Bartl00 <74678008+Bartl00@users.noreply.github.com>
2020-11-24 12:28:33 -0600

    Add files via upload

--- bartlett.py
+++ bartlett.py
@@ -1,1 +1,18 @@
+#This program greets the world and asks the user
+#how they are doing.
+
 print("Hello world")
+userFeeling = input("Are doing well? Y/N: ")
+if userFeeling == "Y":
+    print("I am glad that you are doing well.")
+elif userFeeling == "N":
+    print("That is too bad. I hope things get better for you soon!")
+else:
+    while userFeeling != "Y" or userFeeling != "N":
+        userFeeling = input("I am not sure how you feel. Are you doing well? Y/N ")
+        if userFeeling == "Y":
+            print("I am glad that you are doing well.")
+            break
+        elif userFeeling == "N":
+            print("That is too bad. I hope things get better for you soon!")
+            break
